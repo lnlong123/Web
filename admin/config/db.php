2b71cbe3cdb0639e2aa1f872d93109cde35d1c59
lnlong123 <80403440+lnlong123@users.noreply.github.com>
2021-08-23 19:59:25 +0700

    Add files via upload

---
+++ admin/config/db.php
@@ -0,0 +1,11 @@
+<?php
+    define('HOST','localhost');
+    define('USER','root');
+    define('PASS','');
+    define('DB_NAME','dhtl');
+    $conn = mysqli_connect(HOST,USER,PASS,DB_NAME);
+    mysqli_set_charset($conn, 'UTF8');
+    if(!$conn){
+        die("Không thể kết nối: ".mysqli_connect_error());
+    }
+    ?>
