553932bf137da1e26d76e43f22a2ff6d1d3acbd6
lnlong123 <80403440+lnlong123@users.noreply.github.com>
2021-08-24 08:21:31 +0700

    Add files via upload

--- admin/delete_dv.php
+++ admin/delete_dv.php
@@ -7,8 +7,12 @@
     $id_can_xoa = $_GET['id'];    
     $sql = "DELETE FROM tbl_coquan WHERE id ='$id_can_xoa'";
     $result = mysqli_query($conn,$sql);
+    $sql1="DELETE FROM tbl_canbo WHERE id_donvi='$id_can_xoa'";
+    $result1 = mysqli_query($conn,$sql1);
     if($result == true){
+        if($result1 == true){
         header('Location: indexpb.php');
+        }
     }
 
 ?>
